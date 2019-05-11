4e1f33cb78ce95ff6a0b3086f8552cc00bec4eb8
Cassie Rosner <cerosner@gmail.com>
2019-05-10 22:29:40 -0400

    feat: option selector functionality rendering on DOM

--- workshop-practice/PairExercise.Pixelate/script.js
+++ workshop-practice/PairExercise.Pixelate/script.js
@@ -1,6 +1,7 @@
 // Your code here
 const table = document.getElementsByTagName('table')[0]
-const makeRow = () => {
+
+function makeRow() {
   const row = document.createElement('tr')
 
   for (let i = 0; i < 20; i++) row.appendChild(document.createElement('td'))
@@ -10,3 +11,20 @@ const makeRow = () => {
 
 const addRowBtn = document.getElementById('add-row')
 addRowBtn.addEventListener('click', makeRow)
+
+let selectedColor = 'red'
+
+const select = document.getElementsByTagName('select')[0]
+select.addEventListener('change', event => {
+  selectedColor = event.target.value
+})
+
+function colorize(event) {
+  const cell = event.target
+
+  if (cell.tagName === 'TD') {
+    cell.className = (!cell.className.length || cell.className !== selectedColor) ? selectedColor : ''
+  }
+}
+
+table.addEventListener('click', colorize)
